6c92b02e53b4bef9c1854659aa57eaa2db086484
Houssam Dagdague <hdagdagu@c1r3p3.1337.ma>
2023-04-28 19:00:43 +0100

    CPP_Modules

---
+++ CPP_Modules00/ex01/PhoneBook.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 16:55:42 by hdagdagu          #+#    #+#             */
+/*   Updated: 2023/04/28 17:44:48 by hdagdagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#define MAX_CONTACT 8
+using namespace std;
+
+class Contact
+{
+	public:
+		string first_name;
+		string last_name;
+		string nick_name;
+		string phone_number;
+		string darkest_secret;
+};
+
+class PhoneBook
+{
+	private:
+		int num_contacts;
+		Contact Contacts[MAX_CONTACT];
+	public:
+		friend char *print_int(std::string print);
+		friend void	init_PhoneBook(PhoneBook *phone);
+		friend std::string input();
+		friend void	addContact(PhoneBook *phone);
+		friend void Search_Contact(PhoneBook *phone);
+		friend int getInput_v2(std::string prompt,PhoneBook *phone);
+};
