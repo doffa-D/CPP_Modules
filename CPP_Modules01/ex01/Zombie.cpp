726f08071a055f4bb4d2de7179c0921dfe749b29
doffa-D <hossam.dagdag@gmail.com>
2023-04-30 22:35:58 +0100

    add ex01 && ex02

--- CPP_Modules01/ex01/Zombie.cpp
+++ CPP_Modules01/ex01/Zombie.cpp
@@ -1,21 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 17:41:53 by hdagdagu          #+#    #+#             */
-/*   Updated: 2023/04/30 17:55:30 by hdagdagu         ###   ########.fr       */
+/*   Created: 2023/04/30 17:03:59 by hdagdagu          #+#    #+#             */
+/*   Updated: 2023/04/30 20:15:22 by hdagdagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "Zombie.hpp"
 
-int main()
+Zombie::~Zombie()
 {
-    Zombie* zombie_ptr = Zombie::newZombie("Alix");
-    zombie_ptr->announce();
-    randomChump("James");
-    delete zombie_ptr;
+    std::cout << "Zombie " << this->name << " has been destroyed." << std::endl;
 }
+
+void    Zombie::set_name(std::string name)
+{
+    this->name = name;
+}
+
+void    Zombie::announce()
+{
+    std::cout << this->name << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
--- CPP_Modules01/ex01/zombieHorde.cpp
+++ CPP_Modules01/ex01/zombieHorde.cpp
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 17:41:53 by hdagdagu          #+#    #+#             */
-/*   Updated: 2023/04/30 17:55:30 by hdagdagu         ###   ########.fr       */
+/*   Created: 2023/04/30 19:33:06 by hdagdagu          #+#    #+#             */
+/*   Updated: 2023/04/30 20:07:23 by hdagdagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int main()
+Zombie* zombieHorde( int n, std::string name )
 {
-    Zombie* zombie_ptr = Zombie::newZombie("Alix");
-    zombie_ptr->announce();
-    randomChump("James");
-    delete zombie_ptr;
+    Zombie* horde = new Zombie[n];
+    for(int i = 0; i < num_zombie ;i++)
+    {
+        horde[i].set_name(name);
+    }
+    return horde;
 }
